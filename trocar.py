459e22eea1bc28154adfcaad73970376395597e9
João Miguel <joaomiguelmorais1108@gmail.com>
2024-11-06 21:55:37 -0300

    Update trocar.py

--- trocar.py
+++ trocar.py
@@ -1,15 +1,15 @@
 import random
 
-
+receba = 0
 tentativas = 5
 tentativas2 = 5
 email1 = "a" 
 esqueceu = 0 
 senha1 = "macaco123"
-trocar = 0
+trocar = []
 tentativas3 = 0
 
-
+print(senha1)
 
 verificado = ["1234", "4321", "1357", "4444"]
 numeros = random.choice(verificado)
@@ -34,31 +34,46 @@ while tentativas > 0 and esqueceu != "sim":
         print("Senha incorreta")
         esqueceu = str(input("esqueceu sua senha?"))
 
-if esqueceu == "sim":
-    
+while esqueceu == "sim":
     
     print("um codigo de verificação foi enviado no seu email")
     print(numeros)
     receba = str(input("Você recebeu o código?"))
+    if receba == "sim":
+        break
     
-if receba == "sim":
+while receba == "sim":
     codigo = int(input("Digite o codigo"))
-    
-elif codigo == 1234 and codigo == 4321 and codigo == 4444 and codigo == 1357 :
-    trocar = input("Digite uma nova senha")
-    senha1 = trocar
-    
-while trocar == senha1:
-    
+    if codigo == 1234:
+        trocar = input("Digite uma nova senha")
+        senha1 = trocar
+        break
+    elif codigo == 4444:
+        trocar = input("Digite uma nova senha")
+        senha1 = trocar
+        break
+    elif codigo == 4321:
+        trocar = input("Digite uma nova senha")
+        senha1 = trocar
+        break
+    elif codigo == 1357:
+        trocar = input("Digite uma nova senha")
+        senha1 = trocar
+        print(senha1)
+        
+        break
+while senha1 == trocar:
+    print(senha1)
     senha2 = (input("Digite sua senha novamente"))
     
-    if senha2 == trocar:
+    
+    if senha2 == senha1:
         print("Senha correta")
         break
     else:
         tentativas3 -= 1
         print("Senha incorreta")
-        esqueceu = str(input("esqueceu sua senha novamente?"))
+        esqueceu = str(input("esqueceu sua senha novamente? burro"))
 
 
         
